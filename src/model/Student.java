11771e8f6fe997139e798ae615c809403b492a0c
purnashreek5 <purnashreek5@gmail.com>
2025-12-23 23:09:30 +0530

    Added Student model class with encapsulation

---
+++ src/model/Student.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Student {
+    private String name;
+    private String srn;
+    private String email;
+    private int[] marks;
+    private double percentage;
+    private String grade;
+    private String result;
+}
